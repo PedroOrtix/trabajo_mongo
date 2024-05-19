9611c28fe602ebc3ee5ae1c5eac564e365ce6cf9
PedroOrtix <pedro.ortiz.uni@gmail.com>
2024-05-19 15:26:21 +0200

    refactor: Improve code readability and performance of get_dungeons and get_dungeon_by_id functions

--- queries_folder/get_queries.py
+++ queries_folder/get_queries.py
@@ -1,55 +1,95 @@
 import json
 
 def get_loot(db):
-    # Cogemos todos los objetos de la colección loot
+    """
+    Retrieves all loot items from the database.
+
+    Args:
+        db (Database): The database connection object.
+
+    Returns:
+        list: A list of dictionaries, each containing the 'id' and 'name' of a loot item.
+    """
     loot_items = list(db.loot.find({}, {'_id': 0, 'id': 1, 'name': 1}))
     return loot_items
 
+
 def get_loot_by_id(db, loot_id):
-    # Coger el objeto loot por su id
+    """
+    Retrieves a specific loot item by its ID from the database.
+
+    Args:
+        db (Database): The database connection object.
+        loot_id (int): The ID of the loot item to retrieve.
+
+    Returns:
+        dict: A dictionary containing the details of the loot item, with 'amount' omitted from 'in_rooms'.
+    """
     loot = db.loot.find_one({'id': loot_id}, {'_id': 0})
-    
-    # vamos a omitir el atributo amount dentro de in_rooms
+
     if loot.get('in_rooms') is not None:
         loot['in_rooms'] = [{'room_id': room['room_id'],
                             'room_name': room['room_name'],
                             'dungeon_id': room['dungeon_id'],
-                            'dungeon_name': room['dungeon_name'] } for room in loot['in_rooms']]
-    
+                            'dungeon_name': room['dungeon_name']} for room in loot['in_rooms']]
+
     return loot
-    
+
 
 def get_monster(db):
+    """
+    Retrieves all monsters from the database.
+
+    Args:
+        db (Database): The database connection object.
+
+    Returns:
+        list: A list of dictionaries, each containing the 'id', 'name', 'level', and 'type' of a monster.
+    """
     monsters = list(db.monsters.find({}, {'_id': 0, 'id': 1, 'name': 1, 'level': 1, 'type': 1}))
     return monsters
 
+
 def get_monster_by_id(db, monster_id):
-    # Cogemos el monstruo por su id
+    """
+    Retrieves a specific monster by its ID from the database.
+
+    Args:
+        db (Database): The database connection object.
+        monster_id (int): The ID of the monster to retrieve.
+
+    Returns:
+        dict: A dictionary containing the details of the monster, with 'amount' omitted from 'in_rooms'.
+    """
     monster = db.monsters.find_one({'id': monster_id}, {'_id': 0})
-    # vamos a omitir el atributo amount dentro de in_rooms
+
     if monster.get('in_rooms') is not None:
         monster['in_rooms'] = [{'room_id': room['room_id'],
-                            'room_name': room['room_name'],
-                            'dungeon_id': room['dungeon_id'],
-                            'dungeon_name': room['dungeon_name'] } for room in monster['in_rooms']]
+                                'room_name': room['room_name'],
+                                'dungeon_id': room['dungeon_id'],
+                                'dungeon_name': room['dungeon_name']} for room in monster['in_rooms']]
     return monster
 
+
 def get_dungeons(db):
-    # Aggregate pipeline para agrupar por dungeon_id y dungeon_name
+    """
+    Retrieves a list of all dungeons from the database.
+
+    Args:
+        db (Database): The database connection object.
+
+    Returns:
+        str: A JSON string representing a list of dictionaries, each containing 'dungeon_id' and 'name'.
+    """
     pipeline = [
         {
             "$group": {
-                "_id": "$dungeon_id", # este id hace referencia a la instaciua dungeon_id de la coleccion rooms
-                
-                # vamos a tomar el primer valor de dungeon_id y dungeon_name, suponemos que los ids son unicos
-                # y coger el primero es coger todas sus ocurrencias
+                "_id": "$dungeon_id",
                 "dungeon_id": {"$first": "$dungeon_id"},
                 "name": {"$first": "$dungeon_name"},
-                
             }
         },
         {
-            # estos son los atributos que vamos proyectar
             "$project": {
                 "_id": 0,
                 "dungeon_id": 1,
@@ -60,31 +100,39 @@ def get_dungeons(db):
     dungeons = list(db.rooms.aggregate(pipeline))
     return json.dumps(dungeons)
 
+
 def get_dungeon_by_id(db, dungeon_id):
+    """
+    Retrieves detailed information about a specific dungeon by its ID.
+
+    Args:
+        db (Database): The database connection object.
+        dungeon_id (int): The ID of the dungeon to retrieve.
+
+    Returns:
+        list: A list containing a single dictionary with detailed information about the dungeon and its rooms.
+    """
     pipeline = [
-        # Filtrar rooms por dungeon_id
         {"$match": {"dungeon_id": dungeon_id}},
-        
-        # Agrupar por dungeon_id para consolidar la información
         {"$group": {
             "_id": "$dungeon_id",
-            "name": {"$first": "$dungeon_name"}, # la misma razón que en la función anterior
+            "name": {"$first": "$dungeon_name"},
             "rooms": {"$push": {
                 "room_id": "$room_id",
                 "room_name": "$room_name",
                 "connected_rooms": "$rooms_connected",
                 "monsters": {
-                    "$map": {  # Transforma cada monstruo en el array monsters para obtener solo id y nombre
+                    "$map": {
                         "input": "$monsters",
                         "as": "monster",
-                        "in": { # ponemos el doble $$ porque hacemos referencia a una variable de la variable
+                        "in": {
                             "id": "$$monster.id",
                             "name": "$$monster.name"
                         }
                     }
-                },  # Tenemos que sacar aun el id y nombre de los monstruos
+                },
                 "loots": {
-                    "$map": {  # Transforma cada loot en el array loots para obtener solo id y nombre
+                    "$map": {
                         "input": "$loot",
                         "as": "loot",
                         "in": {
@@ -92,76 +140,72 @@ def get_dungeon_by_id(db, dungeon_id):
                             "name": "$$loot.name"
                         }
                     }
-                },  # Tenemos que sacar aun el id y nombre de los loots
-                    "bug": {"$sum": {"$cond": [{"$eq": ["$hints.category", "bug"]}, 1, 0]}},
-                    "hint": {"$sum": {"$cond": [{"$eq": ["$hints.category", "hint"]}, 1, 0]}},
-                    "lore": {"$sum": {"$cond": [{"$eq": ["$hints.category", "lore"]}, 1, 0]}},
-                    "suggestion": {"$sum": {"$cond": [{"$eq": ["$hints.category", "suggestion"]}, 1, 0]}},
-                }
+                },
+                "bug": {"$sum": {"$cond": [{"$eq": ["$hints.category", "bug"]}, 1, 0]}},
+                "hint": {"$sum": {"$cond": [{"$eq": ["$hints.category", "hint"]}, 1, 0]}},
+                "lore": {"$sum": {"$cond": [{"$eq": ["$hints.category", "lore"]}, 1, 0]}},
+                "suggestion": {"$sum": {"$cond": [{"$eq": ["$hints.category", "suggestion"]}, 1, 0]}},
             }}
-        },
-        
-        # Seleccionar los campos a mostrar
+        }},
         {"$project": {
             "_id": 0,
             "id": "1",
             "name": 1,
             "rooms": 1
-        }   
-        }
+        }}
     ]
-        
-        
+
     result = list(db.rooms.aggregate(pipeline))
-    
-    # en ves que sobrecragar mas el pipeline vamos a hacer una función que quite los duplicados en python
+
     def remove_duplicates(rooms):
         for room in rooms:
-            # Verificar si 'monsters' y 'loots' están presentes y son listas
             if 'monsters' in room and isinstance(room['monsters'], list):
                 unique_monsters = {}
                 for monster in room['monsters']:
-                    if 'id' in monster:  # Asegurarse de que 'id' está presente
+                    if 'id' in monster:
                         unique_monsters[monster['id']] = monster
                 room['monsters'] = list(unique_monsters.values())
 
             if 'loots' in room and isinstance(room['loots'], list):
                 unique_loots = {}
                 for loot in room['loots']:
-                    if 'id' in loot:  # Asegurarse de que 'id' está presente
+                    if 'id' in loot:
                         unique_loots[loot['id']] = loot
                 room['loots'] = list(unique_loots.values())
 
         return rooms
-    
+
     for dungeon in result:
         dungeon['rooms'] = remove_duplicates(dungeon['rooms'])
-        
+
     return result
 
+
 def get_room_by_id(db, room_id):
-    
+    """
+    Retrieves detailed information about a specific room by its ID.
+
+    Args:
+        db (Database): The database connection object.
+        room_id (int): The ID of the room to retrieve.
+
+    Returns:
+        list: A list containing a single dictionary with detailed information about the room, its monsters, loots, and hints.
+    """
     pipeline = [
-        # Filtrar para obtener solo la habitación con el ID específico
         {"$match": {"room_id": room_id}},
-        
-        # Unir con la colección de monstruos si es necesario, asumiendo una colección separada
         {"$lookup": {
             "from": "monsters",
-            "localField": "monsters",  # Asume un campo que contiene IDs de monstruos en la habitación
+            "localField": "monsters",
             "foreignField": "monster_id",
             "as": "monster_details"
         }},
-        
-        # Unir con la colección de tesoros si es necesario, asumiendo una colección separada
         {"$lookup": {
             "from": "loots",
-            "localField": "loot",  # Asume un campo que contiene IDs de tesoros en la habitación
+            "localField": "loot",
             "foreignField": "loot_id",
             "as": "loot_details"
         }},
-        
-        # Proyectar la información necesaria
         {"$project": {
             "_id": 0,
             "idR": "$room_id",
@@ -201,16 +245,34 @@ def get_room_by_id(db, room_id):
         }}
     ]
 
-    # Ejecutar el pipeline de agregación
     result = list(db.rooms.aggregate(pipeline))
     return result
 
+
 def get_user(db):
-    # Coger todos los usuarios, pero solo mostrar email, user_name y country
+    """
+    Retrieves all users from the database.
+
+    Args:
+        db (Database): The database connection object.
+
+    Returns:
+        list: A list of dictionaries, each containing 'email', 'user_name', and 'country' of a user.
+    """
     users = list(db.users.find({}, {'_id': 0, 'email': 1, 'user_name': 1, 'country': 1}))
     return users
 
+
 def get_user_by_email(db, email):
-    # Coger un usuario por su email
+    """
+    Retrieves a specific user by their email from the database.
+
+    Args:
+        db (Database): The database connection object.
+        email (str): The email of the user to retrieve.
+
+    Returns:
+        dict: A dictionary containing the details of the user.
+    """
     user = db.users.find_one({'email': email}, {'_id': 0})
     return user
--- queries_folder/ppd_queries.py
+++ queries_folder/ppd_queries.py
@@ -2,16 +2,27 @@ from datetime import datetime
 
 ### POST QUERIES ###
 def post_comment(db, user_email, room_id, text, category):
-    # tenemos que verificar que el user_email exista
+    """
+    Adds a comment to a specific room and user.
+
+    Args:
+        db (Database): The database connection object.
+        user_email (str): The email of the user posting the comment.
+        room_id (int): The ID of the room to post the comment in.
+        text (str): The content of the comment.
+        category (str): The category of the comment (e.g., bug, hint, lore, suggestion).
+
+    Returns:
+        dict: A status message indicating success or failure.
+    """
     user = db.users.find_one({"email": user_email})
     if not user:
         return {"status": "error", "message": "User does not exist"}
-    
-    # tennemos que verificar que la room_id exista
+
     room = db.rooms.find_one({"room_id": room_id})
     if not room:
         return {"status": "error", "message": "Room does not exist"}
-    
+
     new_hint = {
         "text": text,
         "category": category,
@@ -23,14 +34,12 @@ def post_comment(db, user_email, room_id, text, category):
             "dungeon_name": room["dungeon_name"]
         }
     }
-    
+
     db.users.update_one(
         {"email": user_email},
         {"$push": {"hints": new_hint}}
     )
-    
-    # ahora tenemos que actualizar la colleciuon de rooms "hints"
-    
+
     db.rooms.update_one(
         {"room_id": room_id},
         {"$push": {"hints": {
@@ -49,6 +58,21 @@ def post_comment(db, user_email, room_id, text, category):
     return {"status": "success", "message": "Comment added"}
     
 def post_monster(db, name, type, level, place, exp, man_page):
+    """
+    Adds a new monster to the database.
+
+    Args:
+        db (Database): The database connection object.
+        name (str): The name of the monster.
+        type (str): The type of the monster.
+        level (int): The level of the monster.
+        place (str): The place where the monster can be found.
+        exp (int): The experience points the monster gives.
+        man_page (str): The manual page reference for the monster.
+
+    Returns:
+        dict: A status message indicating success.
+    """
     monster = {
         'name': name,
         'type': type,
@@ -61,6 +85,20 @@ def post_monster(db, name, type, level, place, exp, man_page):
     return {'status': 'success', 'message': 'Monster added'}
 
 def post_loot(db, name, type1, type2, weight, gold):
+    """
+    Adds new loot to the database.
+
+    Args:
+        db (Database): The database connection object.
+        name (str): The name of the loot.
+        type1 (str): The primary type of the loot.
+        type2 (str): The secondary type of the loot.
+        weight (float): The weight of the loot.
+        gold (int): The gold value of the loot.
+
+    Returns:
+        dict: A status message indicating success.
+    """
     loot = {
         'name': name,
         'type1': type1,
@@ -72,6 +110,22 @@ def post_loot(db, name, type1, type2, weight, gold):
     return {'status': 'success', 'message': 'Loot added'}
 
 def post_room(db, dungeon_id, dungeon_name, dungeon_lore, room_name, rooms_connected, inWP=None, outWP=None):
+    """
+    Adds a new room to the database and updates connections with other rooms.
+
+    Args:
+        db (Database): The database connection object.
+        dungeon_id (int): The ID of the dungeon the room belongs to.
+        dungeon_name (str): The name of the dungeon the room belongs to.
+        dungeon_lore (str): The lore of the dungeon.
+        room_name (str): The name of the room.
+        rooms_connected (list): A list of room IDs that are connected to this room.
+        inWP (str, optional): The waypoint to enter the room.
+        outWP (str, optional): The waypoint to exit the room.
+
+    Returns:
+        dict: A status message indicating success.
+    """
     room = {
         'room_id': db.rooms.count_documents({}) + 1,
         'dungeon_id': dungeon_id,
@@ -86,7 +140,6 @@ def post_room(db, dungeon_id, dungeon_name, dungeon_lore, room_name, rooms_conne
     }
     db.rooms.insert_one(room)
     
-    # por cada room conectada, tenemos que agregar la referencia a la nueva room
     for rid in rooms_connected:
         db.rooms.update_one(
             {'room_id': rid},
@@ -97,15 +150,23 @@ def post_room(db, dungeon_id, dungeon_name, dungeon_lore, room_name, rooms_conne
 
 ### PUT QUERIES ###
 def put_room_monsters(db, room_id, monsters):
-    # verificamos si los monsters existen
+    """
+    Updates the list of monsters in a specific room.
+
+    Args:
+        db (Database): The database connection object.
+        room_id (int): The ID of the room to update.
+        monsters (list): A list of monster IDs to add to the room.
+
+    Returns:
+        dict: A status message indicating success or failure.
+    """
     existing_monsters = db.monsters.find({'id': {'$in': monsters}}, {'_id': 0, 'id': 1})
     existing_monsters_ids = [m['id'] for m in existing_monsters]
     
-    # si la cantida de monsters que existe es diferente a la cantidad de monsters que se quieren agregar, raise error
     if len(existing_monsters_ids) != len(monsters):
         return {'status': 'error', 'message': 'One or more monsters do not exist'}
     
-    # Update the room with new monsters
     db.rooms.update_one(
         {'room_id': room_id},
         {'$set': {'monsters': [db.monsters.find_one({'id': mid}, {'_id': 0, 'in_rooms': 0})
@@ -113,14 +174,23 @@ def put_room_monsters(db, room_id, monsters):
     )
 
 def put_room_loot(db, room_id, loot):
-    # verificamos si los loot existen
+    """
+    Updates the list of loot items in a specific room.
+
+    Args:
+        db (Database): The database connection object.
+        room_id (int): The ID of the room to update.
+        loot (list): A list of loot item IDs to add to the room.
+
+    Returns:
+        dict: A status message indicating success or failure.
+    """
     existing_loot = db.loot.find({'id': {'$in': loot}}, {'_id': 0, 'id': 1})
     existing_loot_ids = [lt['id'] for lt in existing_loot]
     
     if len(existing_loot_ids) != len(loot):
         return {'status': 'error', 'message': 'One or more loot items do not exist'}
 
-    # hacer update de la room con el nuevo loot
     db.rooms.update_one(
         {'room_id': room_id},
         {'$set': {'loot': [db.loot.find_one({'id': lid}, {'_id': 0, 'in_rooms': 0})
@@ -128,14 +198,23 @@ def put_room_loot(db, room_id, loot):
     )
 
 def put_room_connections(db, room_id, connections):
-    # Verificar que las rooms existan
+    """
+    Updates the connections of a specific room with other rooms.
+
+    Args:
+        db (Database): The database connection object.
+        room_id (int): The ID of the room to update.
+        connections (list): A list of room IDs to connect to.
+
+    Returns:
+        dict: A status message indicating success or failure.
+    """
     existing_rooms = db.rooms.find({'room_id': {'$in': connections}}, {'_id': 0, 'room_id': 1})
     existing_room_ids = [r['room_id'] for r in existing_rooms]
     
     if len(existing_room_ids) != len(connections):
         return {'status': 'error', 'message': 'One or more connected rooms do not exist'}
 
-    # hacer update de la room con las nuevas conexiones
     db.rooms.update_one(
         {'room_id': room_id},
         {'$set': {'rooms_connected': [db.rooms.find_one({'room_id': rid}, {'_id': 0, 'room_id': 1, 'room_name': 1})
@@ -146,11 +225,20 @@ def put_room_connections(db, room_id, connections):
 
 ### DELETE QUERIES ###
 def delete_room(db, room_id):
-    # delete room and remove references from monsters and loot
+    """
+    Deletes a specific room and removes references to it from monsters and loot.
+
+    Args:
+        db (Database): The database connection object.
+        room_id (int): The ID of the room to delete.
+
+    Returns:
+        dict: A status message indicating success.
+    """
     db.rooms.delete_one({'room_id': room_id})
     db.monsters.update_many(
         {'in_rooms.room_id': room_id}, 
-        {'$pull': {'in_rooms': {'room_id': room_id}}} # es el equivalente a un pop
+        {'$pull': {'in_rooms': {'room_id': room_id}}}
     )
     db.loot.update_many(
         {'in_rooms.room_id': room_id},
@@ -159,6 +247,16 @@ def delete_room(db, room_id):
     return {'status': 'success', 'message': 'Room and related comments deleted'}
 
 def delete_monster(db, monster_id):
+    """
+    Deletes a specific monster and removes references to it from rooms.
+
+    Args:
+        db (Database): The database connection object.
+        monster_id (int): The ID of the monster to delete.
+
+    Returns:
+        dict: A status message indicating success.
+    """
     db.monsters.delete_one({'id': monster_id})
     db.rooms.update_many(
         {'monsters.id': monster_id},
@@ -167,6 +265,16 @@ def delete_monster(db, monster_id):
     return {'status': 'success', 'message': 'Monster deleted'}
 
 def delete_loot(db, loot_id):
+    """
+    Deletes a specific loot item and removes references to it from rooms.
+
+    Args:
+        db (Database): The database connection object.
+        loot_id (int): The ID of the loot item to delete.
+
+    Returns:
+        dict: A status message indicating success.
+    """
     db.loot.delete_one({'id': loot_id})
     db.rooms.update_many(
         {'loot.id': loot_id},
